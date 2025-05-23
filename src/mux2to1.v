25d92ee302e405e28b98c4bac62f68617e84d604
amithraj1823 <amithrajsn.ec22@rvce.edu.in>
2025-05-23 15:51:16 +0530

    Update mux2to1.v

--- src/mux2to1.v
+++ src/mux2to1.v
@@ -1,1 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.05.2025 15:46:21
+// Design Name: 
+// Module Name: mux2to1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2to1 (
+    input wire a,      // Input 0
+    input wire b,      // Input 1
+    input wire sel,    // Select line
+    output wire y      // Output
+);
+    assign y = sel ? b : a;
+endmodule
+
 
