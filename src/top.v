c6e3cd9267b2cc778c4ed6806d7e47ea98db3b26
amithraj1823 <amithrajsn.ec22@rvce.edu.in>
2025-05-25 13:52:44 +0530

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,1 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.05.2025 16:06:08
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input sel,
+    input [7:0] inp,
+    input [7:0] key,
+    output [7:0] out
+);
+
+    wire [7:0] encrypted_out;
+    wire [7:0] decrypted_out;
+
+    encrypt enc (
+        .key(key),
+        .inp(inp),
+        .out(encrypted_out)
+    );
+
+    decrypt dec (
+        .key(key),
+        .inp(inp),
+        .out(decrypted_out)
+    );
+
+    assign out = sel ? encrypted_out : decrypted_out;
+
+endmodule
+
+
 
